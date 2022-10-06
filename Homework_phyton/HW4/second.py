9d71885ffcf9cae1553650e41aef8d955d9f1403
misha1potapenko <misha1potapenko@yandex.ru>
2022-10-06 19:58:16 +0300

    added solution for second task

--- Homework_phyton/HW4/second.py
+++ Homework_phyton/HW4/second.py
@@ -2,3 +2,19 @@
 # Напишите программу, которая составит список простых множителей числа N.
 # *Пример*
 # - при N=236     ->        [2, 2, 59]
+
+
+def mnoj():
+    number = int(input("Введите число N = "))
+    lst_num = []
+    k = 2
+    while number != 1:
+        if number % k == 0:
+            lst_num.append(k)
+            number /= k
+        else:
+            k += 1
+    print(lst_num)
+
+
+mnoj()
