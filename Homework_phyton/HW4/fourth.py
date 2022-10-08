a55db791972ab9ab79e5e1e9653acd39b27d65db
misha1potapenko <misha1potapenko@yandex.ru>
2022-10-08 20:32:07 +0300

    added new solution

--- Homework_phyton/HW4/fourth.py
+++ Homework_phyton/HW4/fourth.py
@@ -19,10 +19,11 @@ k = int(input('Введите натуральную степень k:'))
 
 a_b_c = [randint(0, 100) for i in range(k+1)]
 print(a_b_c)
-poly = '+'.join([f'{j}x^{i}' for i, j in enumerate(a_b_c) if j][::-1])
-
+poly = '+'.join([f'{(j,"")[j==1]}x^{i}' for i, j in enumerate(a_b_c) if j][::-1])
 poly = poly.replace('x^1+', 'x+')
 poly = poly.replace('x^0', '')
+poly += ('', '1')[poly[-1] == '+']
+poly = (poly, poly[:-2])[poly[-2:] == '^1']
 print(poly, "= 0")
 f = open('new_file.txt', 'w')
 f.write(f'{poly} = 0')
