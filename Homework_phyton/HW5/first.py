a9bf9e63e99bf344cb10a69f33905a567759f4cb
misha1potapenko <misha1potapenko@yandex.ru>
2022-10-09 22:34:18 +0300

    added HW

--- Homework_phyton/HW5/first.py
+++ Homework_phyton/HW5/first.py
@@ -1,8 +1,8 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
-with open("HW_5.txt", "w") as data:
+with open("HW.txt", "w") as data:
     data.write("abc dfgsdabc jfhjkhj fsafas rttrey uyutyu abcdsgfg abc  ghtryr")
-with open("HW_5.txt", "r") as data:
+with open("HW.txt", "r") as data:
     string = data.readline().split()
 result = list(filter(lambda x: True if x.find("abc") == -1 else False, string))
 print(result)
