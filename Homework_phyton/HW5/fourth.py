e81b7776dea5dd6158fd1cf13fb2eb8dbaab73d3
misha1potapenko <misha1potapenko@yandex.ru>
2022-10-12 21:00:44 +0300

    added task foutrh

---
+++ Homework_phyton/HW5/fourth.py
@@ -0,0 +1,31 @@
+# Реализуйте RLE алгоритм:
+# реализуйте модуль сжатия и восстановления данных.
+
+def cod(txt):
+    count = 1
+    res = ''
+    for i in range(len(txt)-1):
+        if txt[i] == txt[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt)-2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+    return res
+
+def decod(txt):
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
+
+
+s = input("Введите текст для кодировки: ")
+print(f"Текст после кодировки: {cod(s)}")
+print(f"Текст после дешифровки: {decod(cod(s))}")
