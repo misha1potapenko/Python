9336dd041c08b8d4d72649a1a205103cb2c22235
misha1potapenko <misha1potapenko@yandex.ru>
2022-10-23 11:33:53 +0300

    dictionary

---
+++ Homework_phyton/HW_8/dict.py
@@ -0,0 +1,10 @@
+
+with open("subjects.csv", "r", encoding="UTF-8") as f:
+    for each in f:
+        print(each)
+        subject_dict = dict.fromkeys(f.readlines())
+        print(subject_dict)
+subject_dict['Геометрия\n'] = [3, 4, 5]
+subject_dict["Алгебра\n"] = [4, 3, 2]
+
+print(subject_dict)
