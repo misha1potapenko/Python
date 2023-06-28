35be6e9bc2157d68141794defb5d7d14c5c567e7
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-28 19:15:25 +0300

    added functools_wraps.py

---
+++ functools_wraps.py
@@ -0,0 +1,36 @@
+import time
+from functools import wraps
+
+
+def count(num: int = 1):
+    def deco(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            time_for_count = []
+            result = None
+            for _ in range(num):
+                start = time.perf_counter()
+                result = func(*args, **kwargs)
+                stop = time.perf_counter()
+                time_for_count.append(stop - start)
+            print(f' Результат замеров  = {time_for_count}')
+            return result
+
+        return wrapper
+
+    return deco
+
+
+@count(10)
+def factorial(n):
+    """here must be about factorial"""
+    # print(f'Вычисляю факториал для числа {n}')
+    f = 1
+    for i in range(2, n + 1):
+        f *= i
+    return f
+
+
+print(f'{factorial(1000) = }')
+print(f'{factorial.__name__ = }')
+help(factorial)
