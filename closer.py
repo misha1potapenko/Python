c42b05555da8e4f706adf8c54734c08fdd530ebd
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-28 17:16:32 +0300

    added nonlocal

--- closer.py
+++ closer.py
@@ -11,3 +11,18 @@ def add_one(a):
 hello = add_one('Helolo')
 print(hello("Alex"))
 print(hello('Masha'))
+
+
+def add_one_nonlocal(a):
+
+    text = ''
+
+    def add_two_nonlocal(b):
+        nonlocal text
+        text += ', ' + b
+        return a + text
+    return add_two_nonlocal
+
+hello = add_one_nonlocal('Helolo')
+print(hello("Alex"))
+print(hello('Masha'))
