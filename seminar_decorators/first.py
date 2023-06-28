26b266cde7d87c9492b0a1deb1427d95b320f54e
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-28 19:40:58 +0300

    added seminar for decorator first task

---
+++ seminar_decorators/first.py
@@ -0,0 +1,39 @@
+# Создайте функцию-замыкание, которая запрашивает два целых
+# числа:
+# ○ от 1 до 100 для загадывания,
+# ○ от 1 до 10 для количества попыток
+# Функция возвращает функцию, которая через консоль просит
+# угадать загаданное число за указанное число попыток.
+import random
+import time
+
+
+def count(num: int = 1):
+    def deco(func):
+        def wrapper(*args, **kwargs):
+            result = random.randint(0, 100)
+            print(result)
+            for _ in range(num):
+                if result == func(*args, **kwargs):
+                    print("Yes")
+                    break
+                else:
+                    print("No")
+
+
+            return result
+
+        return wrapper
+
+    return deco
+
+
+
+@count(10)
+def guess():
+    my_guess = int(input("Введите число от 0 до 100  "))
+    return my_guess
+
+
+
+print(guess())
